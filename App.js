1e8e33f4b8a4f436a04925775bc2a5dfc78ef9f5
Goszczyk1878 <bartekkula73@gmail.com>
2026-02-13 14:52:09 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,10 @@
 import './App.css';
+import okon from './okoń.jpg'
+import sandacz from './sandacz.jpg'
+import halibut from './halibut.jpg'
+import tunczyk from './tunczyk.png'
+import wegorz from './wegorz.jpg'
+import mintaj from './mintaj.jpg'
 
 function App() {
   return (
