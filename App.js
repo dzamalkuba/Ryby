7d7220a35ff83e8beb1b0033192866b09fd7b3fe
dzamalkuba <kubakonopinski026@gmail.com>
2026-02-13 14:58:52 +0100

    Dodanie pierwszego skryptu App.js

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
+import { useState } from 'react';
+import halibut from './halibut.jpg'
 import './App.css';
 
 function App() {
+
+  const [okon,setOkon] = useState(false)
+  const [halibut1,setHalibut1] = useState(false)
+
+const pojawienie = () => {
+    setHalibut1(true);
+  };
+
   return (
     <div className="App">
       <h1>Ryby</h1>
-      <p>Dodaje gałązke</p>
+
+      <div className='halibut'>
+          <img src={halibut} alt="najedź na mnie"/>
+      </div>
+
+      <div style={{ padding: '20px', fontFamily: 'Arial' }}>
+      <h1>Informacje:</h1>
+      <button onClick={pojawienie}>Pokaż</button>
+  
+      {halibut1 && <p>Halibut (Hippoglossus hippoglossus) – duża ryba dorszowata, mięsiste białe mięso idealne do pieczenia i grillowania.
+</p>}
+    </div>
+
     </div>
   );
 }
