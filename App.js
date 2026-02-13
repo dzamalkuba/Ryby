8cf7e072cf479764fd5cb50ecf44b2d2b8885f5b
Goszczyk1878 <bartekkula73@gmail.com>
2026-02-13 14:57:54 +0100

    Update App.js

--- App.js
+++ App.js
@@ -10,6 +10,30 @@ function App() {
   return (
     <div className="App">
       <h1>Ryby</h1>
+      <h2>Okoń</h2>
+      <div className="okon">
+      <img src={okon} alt="najedź na mnie1">
+    </div>
+    <h3>Sandacz</h3>
+     <div className="sandacz">
+      <img src={okon} alt="najedź na mnie2">
+    </div>
+    <h4>Węgorz</h4>
+     <div className="wegorz">
+      <img src={okon} alt="najedź na mnie3">
+    </div>
+    <h5>Mintaj</h5>
+     <div className="mintaj">
+      <img src={okon} alt="najedź na mnie4">
+    </div>
+    <h6>Halibut</h6>
+     <div className="halibut">
+      <img src={okon} alt="najedź na mnie5">
+    </div>
+    <h7>Tuńczyk</h7>
+     <div className="tunczyk">
+      <img src={okon} alt="najedź na mnie6">
+    </div>
     </div>
   );
 }
